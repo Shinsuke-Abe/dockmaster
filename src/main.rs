f7cf3c12ab053db98a6690c3e106edd41f5f476c
Shinsuke-Abe <mao.instantlife@gmail.com>
2017-04-17 05:14:59 +0900

    add base directory option to create sub command.

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,25 @@ extern crate rustc_serialize;
 extern crate docopt;
 
 use docopt::Docopt;
+use std::env;
 
 const USAGE: &'static str = "
 Dockmaster.
 
 Usage:
-    dockmaster create <project-name>
+    dockmaster create <project-name> [--base=<base-dir>]
     dockmaster (-h | --help)
     dockmaster --version
 
 Options:
-    -h --help   Show this screen.
-    --version   Show version.
+    --base=<base-dir>   project base directory(default is current directory).
+    -h --help           Show this screen.
+    --version           Show version.
 ";
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
+    flag_base: String,
     arg_project_name: String,
     cmd_create: bool,
 }
@@ -29,5 +32,10 @@ fn main() {
     println!("{:?}", args);
     if args.cmd_create {
         println!("  createing {}", args.arg_project_name);
+        if !args.flag_base.is_empty() {
+            println!("  base directory is {}", args.flag_base);
+        } else {
+            println!("  base directory is {}", env::current_dir().unwrap().display());
+        }
     }
 }
